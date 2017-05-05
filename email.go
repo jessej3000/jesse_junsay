82bb492b2228f2175eb9ebdd2c71510320149a0b
System Administrator <root@Jesses-MacBook-Pro.local>
2017-05-05 18:04:02 +0800

    Reset password updates

--- email.go
+++ email.go
@@ -11,7 +11,6 @@ func sendPasswordResetLink(email string, id int64, code string) {
 	from := "jessejmwp2017@gmail.com"
 	pass := "20mwp17golang"
 
-	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n;"
 	to := email
 	href := "http://view-source:ec2-52-36-191-60.us-west-2.compute.amazonaws.com:8080/resetpassword?id=" + strconv.Itoa(int(id)) + "&code=" + code
 	link := "<a href='" + href + "'>" + href + "</>"
@@ -19,7 +18,9 @@ func sendPasswordResetLink(email string, id int64, code string) {
 
 	msg := "From: " + from + "\n" +
 		"To: " + to + "\n" +
-		"Subject: Password Reset Link\n\n" + mime + body
+		"MIME-Version: 1.0" + "\r\n" +
+		"Content-type: text/html" + "\r\n" +
+		"Subject: Password Reset Link\n\n" + body
 
 	err := smtp.SendMail("smtp.gmail.com:587",
 		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
