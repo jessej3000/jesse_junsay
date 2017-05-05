68daf2dc845cab3dd984d51aa8affcef90c6b2cd
System Administrator <root@Jesses-MacBook-Pro.local>
2017-05-05 18:29:17 +0800

    Reset password updates

---
+++ resetpassword.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+// Description      :     Login handler
+// returns          :     None
+func handleResetPassword(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		f, _ := template.ParseFiles("views/resetpassword.htm")
+		msgMap := r.URL.Query()
+
+		if len(msgMap) > 0 {
+			if val, ok := msgMap["code"]; ok { // Check if code exist
+				// Check if code exist
+				if checkIfCodeExist(val[0]) { //Show reset view
+					ResetID = msgMap["id"][0]
+					f.Execute(w, nil)
+				} else {
+					http.Redirect(w, r, "/login?msg=Reset password link no longer valid.", http.StatusSeeOther)
+				}
+			} else {
+				http.Redirect(w, r, "/login?msg=Reset password link no longer valid.", http.StatusSeeOther)
+			}
+		} else {
+			http.Redirect(w, r, "/login?msg=Reset password link no longer valid.", http.StatusSeeOther)
+		}
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		if updatePassword(ResetID, r.FormValue("password")) { // Login if true
+			http.Redirect(w, r, "/login?msg=Password reset successful", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/login?msg=Unable to reset password", http.StatusSeeOther)
+		}
+	}
+}
