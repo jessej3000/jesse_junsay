bee617d8d4bc55423168bfc37a7fbb0347b8b808
System Administrator <root@Jesses-MacBook-Pro.local>
2017-05-05 17:36:44 +0800

    Reset password updates

--- email.go
+++ email.go
@@ -8,7 +8,8 @@ func sendPasswordResetLink(email string, id int64, code string) {
 	from := "jessejmwp2017@gmail.com"
 	pass := "20mwp17golang"
 	to := email
-	link := "http://view-source:ec2-52-36-191-60.us-west-2.compute.amazonaws.com:8080/resetpassword?id=" + string(id) + "&code=" + code
+	href := "http://view-source:ec2-52-36-191-60.us-west-2.compute.amazonaws.com:8080/resetpassword?id=" + string(id) + "&code=" + code
+	link := "<a href='" + href + "'>" + href + "</>"
 	body := "Please follow the link to reset password: " + link
 
 	msg := "From: " + from + "\n" +
